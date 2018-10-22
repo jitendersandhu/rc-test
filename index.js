17fffe3e5c584e40c556d0df12bccef84edfdd8c
Jitender <jitendersandhu@users.noreply.github.com>
2018-10-22 13:34:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function() {
 	var insertCustomScript = function () {
-		var scriptUrl = 'https://cdn.jsdelivr.net/gh/jitendersandhu/rc-test@4dee347c714376a59cc11a5c9435167991ce4e4a/custom.js',
+		var scriptUrl = 'https://cdn.jsdelivr.net/gh/jitendersandhu/rc-test@642f7bd8bbda3120145b1cbfe319191d5f8d465f/custom.js',
 			body = document.getElementsByTagName('body')[0],
 			scriptTag = document.createElement('script');
 
